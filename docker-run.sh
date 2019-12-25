be3e6da225a717e76fd62876d12e0327a03afd2b
kjchiu <kj.chiu73@gmail.com>
2019-12-25 22:03:56 +0800

    Detach after docker run

--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/zsh
-docker run -p 7777:80 -p 8888:8080 --name ubuntu-4-dev $(cat volume-paths) ubuntu-4-dev
+docker run -d -p 7777:80 -p 8888:8080 --name ubuntu-4-dev $(cat volume-paths) ubuntu-4-dev
